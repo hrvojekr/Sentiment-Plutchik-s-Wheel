aa9f3723d698ae5c3bced3ba86554a98b4a5ca41
hrvojek <hrvoje.krpan@gmail.com>
2018-11-23 23:52:06 +0100

    - Finished Sentiment Analysis

--- sent_plutchik.R
+++ sent_plutchik.R
@@ -9,7 +9,6 @@ library(tidyverse)
 library(tidytext)
 library(xlsx)
 
-
 #Get the data
 WholeConversation <- read.delim("gyppwhole.txt")
 
@@ -21,12 +20,15 @@ nrc <- get_sentiments("nrc")
 #Make DF the same type
 # is.factor(WholeConversation$gypp_text)
 # is.character(nrc$word)
-WholeConversation_Ch <- as.character(WholeConversation$gypp_text)
 
+WholeConversation_Ch <- as.character(WholeConversation$gypp_text)
 WholeConversation_Tibble <- tibble(WholeConversation_Ch)
 
+#Count words that repeat itself
+GyppTibble <- count(WholeConversation_Tibble,WholeConversation_Ch)
 
-gypp_plutchik <- WholeConversation_Tibble %>% 
+
+gypp_plutchik <- GyppTibble %>% 
 # Join to nrc lexicon
 inner_join(nrc, by = c("WholeConversation_Ch" = "word")) %>% 
 # Only consider Plutchik sentiments
@@ -34,6 +36,11 @@ filter(!sentiment %in% c("positive", "negative")) %>%
 # Group by sentiment
 group_by(sentiment) %>% 
 # Get total count by sentiment
-summarize(total_count = sum(count))
+summarize(count = sum(n))
+
+#Plot the data
+ggplot(gypp_plutchik, aes(x = sentiment, y = count)) +
+  # Add a column geom
+  geom_col()
 
 
