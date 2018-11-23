a57c4ab91d518c42a1828a3c9ea4df633237d560
hrvojek <hrvoje.krpan@gmail.com>
2018-11-23 11:19:52 +0100

    -Plutchik Emotion on Whatsup Conversation

--- sent_plutchik.R
+++ sent_plutchik.R
@@ -2,21 +2,38 @@
 # Packges
 install.packages("tidyverse")
 install.packages("tidytext")
+install.packages("xlsx")
 
 #Load Library
 library(tidyverse)
 library(tidytext)
+library(xlsx)
 
-# "NRC" lexicon (Plutchik's Wheel)
 
-sent_plutchik <- get_sentiments("nrc") %>% 
-  count(sentiment)
+#Get the data
+WholeConversation <- read.delim("gyppwhole.txt")
 
-sent_plutchik
 
-# Plot n vs. sentiment
+#Get the nrc lexicon
+nrc <- get_sentiments("nrc")
 
-ggplot(nrc_counts, aes(x = sentiment, y = n)) +
-  # Add a col layer
-  geom_col() +
-  theme_gdocs()
+
+#Make DF the same type
+# is.factor(WholeConversation$gypp_text)
+# is.character(nrc$word)
+WholeConversation_Ch <- as.character(WholeConversation$gypp_text)
+
+WholeConversation_Tibble <- tibble(WholeConversation_Ch)
+
+
+gypp_plutchik <- WholeConversation_Tibble %>% 
+# Join to nrc lexicon
+inner_join(nrc, by = c("WholeConversation_Ch" = "word")) %>% 
+# Only consider Plutchik sentiments
+filter(!sentiment %in% c("positive", "negative")) %>%
+# Group by sentiment
+group_by(sentiment) %>% 
+# Get total count by sentiment
+summarize(total_count = sum(count))
+
+
