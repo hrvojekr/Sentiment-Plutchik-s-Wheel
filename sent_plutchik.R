c79df1d66af96a626f5b353623910ea29869780f
Hrvoje Krpan <hrvoje.krpan@gmail.com>
2019-12-23 14:57:24 +0100

    Setiment Update

--- sent_plutchik.R
+++ sent_plutchik.R
@@ -8,39 +8,41 @@ install.packages("xlsx")
 library(tidyverse)
 library(tidytext)
 library(xlsx)
+library(readxl)
 
 #Get the data
-WholeConversation <- read.delim("gyppwhole.txt")
+testSloSent <- read.delim("tadejtext.txt", fileEncoding = "UTF-8"  )
+#testSloSent <- "Z dodatnimi minutami ste me prijetno presenetili. Res super!"
 
+#Slovenian Dictionary
+sloLex <- read.xlsx("slo_sentiments_dictionary_opt.xlsx", sheetIndex = 1 , encoding="UTF-8")
+tibble(sloLex)
+sloLex <- read_excel("slo_sentiments_dictionary_opt.xlsx")
 
-#Get the nrc lexicon
-nrc <- get_sentiments("nrc")
-
+sloSentList <- as.list(sloLex)
+SloSentTibble <- tibble(sloSentList)
 
 #Make DF the same type
 # is.factor(WholeConversation$gypp_text)
 # is.character(nrc$word)
 
-WholeConversation_Ch <- as.character(WholeConversation$gypp_text)
-WholeConversation_Tibble <- tibble(WholeConversation_Ch)
+WholeConversation_Ch <- as.character(testSloSent$gypp_text)
+WWhWholeConversation_Tibble <- tibble(WholeConversation_Ch)
 
 #Count words that repeat itself
-GyppTibble <- count(WholeConversation_Tibble,WholeConversation_Ch)
+GyppTibbleSlo <- count(WWhWholeConversation_Tibble,WholeConversation_Ch)
 
-
-gypp_plutchik <- GyppTibble %>% 
-# Join to nrc lexicon
-inner_join(nrc, by = c("WholeConversation_Ch" = "word")) %>% 
-# Only consider Plutchik sentiments
-filter(!sentiment %in% c("positive", "negative")) %>%
-# Group by sentiment
-group_by(sentiment) %>% 
-# Get total count by sentiment
-summarize(count = sum(n))
+gypp_plutchik <- GyppTibbleSlo %>% 
+  # Join to nrc lexicon
+  inner_join(sloLex, by = c("WholeConversation_Ch" = "word")) %>% 
+  # Only consider Plutchik sentiments
+  filter(!sentiment %in% c("positive", "negative")) %>%
+  # Group by sentiment
+  group_by(sentiment) %>% 
+  # Get total count by sentiment
+  summarize(count = sum(n))
 
 #Plot the data
 ggplot(gypp_plutchik, aes(x = sentiment, y = count)) +
   # Add a column geom
   geom_col()
-
-
